7f0c5d668e2f5db0c03fbe6d84f1025e78db01fe
QuinnOster <46699216+QuinnOster@users.noreply.github.com>
2019-09-15 23:20:28 -0600

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities {
+
+    public static string ProcessText(string textIn) {
+
+        /*~~~~~ Check if Number/String ~~~~~ */
+        return word 'string' if user enters string
+        //return word 'number' if user enters a number
+        return textIn;
+         
+        if (char.IsNumber(textIn) == true) {
+            return "number";
+        }
+
+       if (char.IsNumber(textIn) == false) {
+            return "string";
+        }
+
+        /*~~~~~ Letter Count Solution ~~~~~ */
+        public static string avTextLength(string str) {
+            string[] words = str.Split();
+        int total = 0;
+        foreach (var word in words) { total += word.Length; }
+        return "" + total / words.Length;    
+
+        }
+
+    }
+}
